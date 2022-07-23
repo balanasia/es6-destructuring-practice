953730e25e62259e6d7ef994f5e8632d6c8a1dea
Anastasiia Balakhan <41988936+balanasia@users.noreply.github.com>
2022-07-23 08:55:31 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+//CHALLENGE: uncomment the code below and see the car stats rendered
+import React from "react";
+import ReactDOM from "react-dom";
+import cars from "./practice.js";
+//import animals from "./data.js/";
+
+console.log(cars);
+
+const [honda, tesla] = cars;
+
+// const [cat, dog] = animals;
+// console.log(cat);
+// console.log(dog);
+
+// const { name, sound } = cat;
+
+// console.log(cat.sound);
+
+// ReactDOM.render(
+//   <table>
+//     <tr>
+//       <th>Brand</th>
+//       <th>Top Speed</th>
+//     </tr>
+//     <tr>
+//       <td>{tesla.model}</td>
+//       <td>{teslaTopSpeed}</td>
+//       <td>{teslaTopColour}</td>
+//     </tr>
+//     <tr>
+//       <td>{honda.model}</td>
+//       <td>{hondaTopSpeed}</td>
+//       <td>{hondaTopColour}</td>
+//     </tr>
+//   </table>,
+//   document.getElementById("root")
+// );
